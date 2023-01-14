8385b3a8e78eb7124f073606084a51d906f1ed02
Amarvin J <amarvin@codesteak.com>
2023-01-14 15:40:16 +0530

    API changed

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -26,10 +26,12 @@ function Convert({ language, text, translated, setTranslated }) {
           params: {
             q: debouncedText,
             target: language.value,
-            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+            key: "AIzaSyDfr8quOrmhLX1yyXJnImr-wRJdit5jWOY",
           },
         }
       );
+
+      //key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
 
       setTranslated(data.data.translations[0].translatedText);
     };
